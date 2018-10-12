c7c8e1e35713a8c7ae50360d6e6a5ac4020fb1c0
igarkhipenko <i.arkhipenko@crystals.ru>
2018-10-12 18:14:22 +0300

    components styling (bootstrap) + css styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ArticleList from './ArticleList'
 import articles from '../fixtures'
+import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
     return (
-       <div>
-           <h1>App name</h1>
-           <ArticleList articles={articles}/>
-       </div>
+        <div className='container'>
+        <div className='jumbotron'>
+            <h1 className='display-3'>App name</h1>
+        </div>
+            <ArticleList articles={articles}/>
+        </div>
    )
 }
 
